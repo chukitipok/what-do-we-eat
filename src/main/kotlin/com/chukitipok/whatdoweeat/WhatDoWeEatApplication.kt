aa3f62a2e23ff308c6ed7c9049b0e5df21eb6106
chukitipok <antonin.texier@gmail.com>
2021-08-10 20:15:13 +0200

    Init Spring project.

---
+++ src/main/kotlin/com/chukitipok/whatdoweeat/WhatDoWeEatApplication.kt
@@ -0,0 +1,11 @@
+package com.chukitipok.whatdoweeat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatDoWeEatApplication
+
+fun main(args: Array<String>) {
+    runApplication<WhatDoWeEatApplication>(*args)
+}
---
+++ src/test/kotlin/com/chukitipok/whatdoweeat/WhatDoWeEatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chukitipok.whatdoweeat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhatDoWeEatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
